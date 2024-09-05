1fa7b2f70ec9fdbad426c83d90eb752664316d7c
Anandha Kannan N <anandha.kannan@ascendo.ai>
2024-09-05 19:10:54 +0530

    Add Signin api

--- models/schemas/auth_schemas.py
+++ models/schemas/auth_schemas.py
@@ -1,7 +1,12 @@
 from models.schemas.common import BaseModel
+from configs.base_config import BaseConfig
 
 
-class RegisterUser(BaseModel):
+class AuthBase(BaseModel):
     email: str
     password: str
     organisation_name: str
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = BaseConfig.SECRET_KEY
--- resources/auth_controller.py
+++ resources/auth_controller.py
@@ -3,17 +3,25 @@ from fastapi.encoders import jsonable_encoder
 from models import get_db
 from models.schemas import auth_schemas
 from sqlalchemy.orm import Session
+from fastapi_jwt_auth import AuthJWT
+
 
 from services.member_services import create_member
 from services.organisation_services import create_organisation, get_organisation_by_name
 from services.user_services import create_user, get_user_by_email
+from configs.base_config import BaseConfig
 
 router = APIRouter()
 
 
+@AuthJWT.load_config
+def get_config():
+    return auth_schemas.Settings()
+
+
 @router.post("/signup")
 def signup(
-    req_body: auth_schemas.RegisterUser,
+    req_body: auth_schemas.AuthBase,
     db: Session = Depends(get_db),
 ):
     db_user = get_user_by_email(db=db, email=req_body.email)
@@ -33,3 +41,38 @@ def signup(
     db.commit()
     db.refresh(db_member)
     return {"message": "success", "data": jsonable_encoder(db_member)}
+
+
+@router.post("/signin")
+def signin(
+    req_body: auth_schemas.AuthBase,
+    db: Session = Depends(get_db),
+    authorize: AuthJWT = Depends(),
+):
+    db_user = get_user_by_email(db=db, email=req_body.email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid User")
+
+    if not db_user.verify_password(req_body.password):
+        raise HTTPException(status_code=400, detail="Password Mismatch")
+
+    if not db_user.status:
+        raise HTTPException(
+            status_code=400,
+            detail="User not yet active. Please contact your Administrator",
+        )
+
+    access_token = authorize.create_access_token(
+        subject=db_user.email,
+        expires_time=BaseConfig.ACCESS_TOKEN_EXPIRY_DELTA,
+    )
+    refresh_token = authorize.create_refresh_token(
+        subject=db_user.email,
+        expires_time=BaseConfig.REFRESH_TOKEN_EXPIRY_DELTA,
+    )
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
