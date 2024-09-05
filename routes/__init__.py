9977e86f68641a285a9306022ab15b7d3e6fff4d
Anandha Kannan N <anandha.kannan@ascendo.ai>
2024-09-05 17:07:03 +0530

    Add router

---
+++ routes/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from resources.auth_controller import router as auth_router
+
+
+router = APIRouter()
+
+
+router.include_router(auth_router, prefix="/auth", tags=["Auth"])
