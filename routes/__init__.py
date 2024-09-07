03d03e9f38fd94ad091df2ca90de384300606d33
Anandha Kannan N <anandha.kannan@ascendo.ai>
2024-09-07 11:26:51 +0530

    adding stat controller

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from resources.auth_controller import router as auth_router
 from resources.member_controller import router as member_router
+from resources.stat_controller import router as stat_router
 
 
 router = APIRouter()
@@ -9,3 +10,4 @@ router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 router.include_router(member_router, prefix="/member", tags=["Member"])
+router.include_router(stat_router, prefix="/stat", tags=["Stat"])
